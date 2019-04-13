d1a0c7957455598e63afb38d248fef48a7120a4c
schlaphi <schlaphi@students.zhaw.ch>
2019-04-13 13:33:06 +0200

    added PurchaseNotFoundException

---
+++ src/main/java/com/dlizarra/starter/purchase/PurchaseNotFoundException.java
@@ -0,0 +1,7 @@
+package com.dlizarra.starter.purchase;
+
+public class PurchaseNotFoundException extends RuntimeException{
+  public PurchaseNotFoundException(final Integer id) {
+    super("Could not find Purchase with id: " + id);
+  }
+}
