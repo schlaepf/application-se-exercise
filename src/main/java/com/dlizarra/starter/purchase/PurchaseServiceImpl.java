6f829cc0f7f9d2a9e139c99cc9708bf5e3e218ae
schlaphi <schlaphi@students.zhaw.ch>
2019-04-13 13:13:45 +0200

    creation of purchase works

--- src/main/java/com/dlizarra/starter/purchase/PurchaseServiceImpl.java
+++ src/main/java/com/dlizarra/starter/purchase/PurchaseServiceImpl.java
@@ -2,9 +2,11 @@ package com.dlizarra.starter.purchase;
 
 import com.dlizarra.starter.support.orika.OrikaBeanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -37,8 +39,13 @@ public class PurchaseServiceImpl implements PurchaseService{
     return null;
   }
 
+  @Transactional(readOnly = true)
   @Override
   public List<PurchaseDto> getPurchases() {
-    return null;
+    final List<Purchase> purchases = purchaseRepository.findAll(new Sort("id"));
+    final List<PurchaseDto> purchaseDto = new ArrayList<PurchaseDto>();
+    purchases.forEach(x -> purchaseDto.add(mapper.map(x, PurchaseDto.class)));
+
+    return purchaseDto;
   }
 }
